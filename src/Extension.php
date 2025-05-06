sampoyigi/ti-ext-telescope

Fix merge config from path

src/Extension.php
@@ -15,7 +15,7 @@ class Extension extends BaseExtension
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/config/telescope.php', 'telescope'
+            __DIR__.'/../config/telescope.php', 'telescope'
         );
 
 //        app()->register(\Illuminate\Auth\AuthServiceProvider::class);

sampoyigi/ti-ext-sentry

src/Extension.php
@@ -12,7 +12,7 @@ class Extension extends BaseExtension
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/config/sentry.php', 'sentry'
+            __DIR__.'/../config/sentry.php', 'sentry'
         );
 
         $this->app->register(\Sentry\Laravel\ServiceProvider::class);